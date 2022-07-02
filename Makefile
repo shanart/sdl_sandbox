95cf91350ab9971161c6d3c0ad784f4ae38432ee
shanart <shanin.x@gmail.com>
2022-07-02 13:36:31 +0300

    initial commit

---
+++ Makefile
@@ -0,0 +1,3 @@
+
+all:
+	g++ src/*.cpp -w -lSDL2 -o ./bin/main
---
+++ src/Game.h
@@ -0,0 +1,29 @@
+#ifndef SHAPE_CPP
+#define SHAPE_CPP
+
+#include <SDL2/SDL.h>
+
+class Game
+{
+public:
+    Game() {}
+    ~Game() {}
+
+    // simply set the dunning variable to true
+    bool init(const char *title, int xpos, int ypos, int width, int height, int flags);
+    void render();
+    void update();
+    void handleEvents();
+    void clean();
+
+    // a function to access the private running variable
+    bool running() { return m_bRunning; }
+
+private:
+    SDL_Window *m_pWindow;
+    SDL_Renderer *m_pRenderer;
+
+    bool m_bRunning;
+};
+
+#endif
